744f30a821650ffebe90c37b646324f681b6d975
Collin Van Dyck <collinvandyck@gmail.com>
2024-01-24 23:04:10 -0500

    Add into convenience parameters.

--- src/coord.rs
+++ src/coord.rs
@@ -2,20 +2,21 @@
 
 use std::ops;
 
-pub fn point(x: Num, y: Num, z: Num) -> Point {
+pub fn point(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>) -> Point {
     Point::new(x, y, z)
 }
 
-pub fn vector(x: Num, y: Num, z: Num) -> Vector {
+pub fn vector(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>) -> Vector {
     Vector::new(x, y, z)
 }
 
-pub fn tuple(x: Num, y: Num, z: Num, w: Num) -> Tuple4 {
+pub fn tuple(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>, w: impl Into<Num>) -> Tuple4 {
     Tuple4::new(x, y, z, w)
 }
 
 type Num = f64;
 
+#[derive(Debug, PartialEq)]
 pub struct Point {
     tup: Tuple4,
 }
@@ -34,13 +35,14 @@ impl ops::DerefMut for Point {
 }
 
 impl Point {
-    pub fn new(x: Num, y: Num, z: Num) -> Self {
+    pub fn new(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>) -> Self {
         Self {
-            tup: Tuple4::new(x, y, z, 0.0),
+            tup: Tuple4::new(x, y, z, 1),
         }
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Vector {
     tup: Tuple4,
 }
@@ -59,18 +61,19 @@ impl ops::DerefMut for Vector {
 }
 
 impl Vector {
-    pub fn new(x: Num, y: Num, z: Num) -> Self {
+    pub fn new(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>) -> Self {
         Self {
-            tup: Tuple4::new(x, y, z, 1.0),
+            tup: Tuple4::new(x, y, z, 0),
         }
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Tuple4([Num; 4]);
 
 impl Tuple4 {
-    pub fn new(x: Num, y: Num, z: Num, w: Num) -> Self {
-        Self([x, y, z, w])
+    pub fn new(x: impl Into<Num>, y: impl Into<Num>, z: impl Into<Num>, w: impl Into<Num>) -> Self {
+        Self([x.into(), y.into(), z.into(), w.into()])
     }
     pub fn x(&self) -> Num {
         self.0[0]
@@ -106,18 +109,29 @@ impl Tuple4 {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Deref;
+
     use super::*;
 
     #[test]
     fn test_tuples() {
-        let t = tuple(4.3, -4.2, 3.1, 1.0);
+        let t = tuple(4.3, -4.2, 3.1, 1);
         assert_eq!([t.x(), t.y(), t.z(), t.w()], [4.3, -4.2, 3.1, 1.0]);
         assert!(t.is_point());
         assert!(!t.is_vector());
 
-        let t = tuple(4.3, -4.2, 3.1, 0.0);
+        let t = tuple(4.3, -4.2, 3.1, 0);
         assert_eq!([t.x(), t.y(), t.z(), t.w()], [4.3, -4.2, 3.1, 0.0]);
         assert!(!t.is_point());
         assert!(t.is_vector());
+    }
+
+    #[test]
+    fn test_tuple_convenience() {
+        let p = point(4, -4, 3);
+        assert_eq!(p.deref(), &tuple(4, -4, 3, 1));
+
+        let v = vector(4, -4, 3);
+        assert_eq!(v.deref(), &tuple(4, -4, 3, 0));
     }
 }
