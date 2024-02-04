b6a6d0e58e28e2f29321625aed948ffe41550840
Collin Van Dyck <collinvandyck@gmail.com>
2024-02-04 12:54:47 -0500

    Test matrixes not equal.

--- src/matrix.rs
+++ src/matrix.rs
@@ -121,6 +121,25 @@ mod tests {
         assert_eq!(ma, mb);
     }
 
+    #[test]
+    fn test_matrix_equality_with_different_matrices() {
+        let ma = matrix!(
+            "
+            | 1 | 2 | 3 | 4 |
+            | 5 | 6 | 7 | 8 |
+            | 9 | 8 | 7 | 6 |
+            | 5 | 4 | 3 | 2 | "
+        );
+        let mb = matrix!(
+            "
+            | 2 | 3 | 4 | 5 |
+            | 6 | 7 | 8 | 9 |
+            | 8 | 7 | 6 | 5 |
+            | 4 | 3 | 2 | 1 |"
+        );
+        assert_ne!(ma, mb);
+    }
+
     fn matrix_from_spec(spec: &str) -> anyhow::Result<Matrix> {
         let vals = spec
             .split('|')
