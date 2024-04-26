2959f47fa3cc99116e4749e48bedf73285cf09ab
Collin Van Dyck <collinvandyck@gmail.com>
2024-04-26 17:52:39 -0400

    Implement Ray::transform.

--- src/rays.rs
+++ src/rays.rs
@@ -19,6 +19,13 @@ impl Ray {
         self.origin.add_vector(dir)
     }
 
+    pub fn transform(&self, m: Matrix) -> Self {
+        Self {
+            origin: m.mul_point(self.origin),
+            dir: m.mul_vector(self.dir),
+        }
+    }
+
     pub fn origin(&self) -> Point {
         self.origin
     }
@@ -48,5 +55,23 @@ mod tests {
         assert_eq!(r.position(1), point(3, 3, 4));
         assert_eq!(r.position(-1), point(1, 3, 4));
         assert_eq!(r.position(2.5), point(4.5, 3, 4));
+    }
+
+    #[test]
+    fn translating_a_ray() {
+        let r = ray(point(1, 2, 3), vector(0, 1, 0));
+        let m = translation(3, 4, 5);
+        let r2 = r.transform(m);
+        assert_eq!(r2.origin, point(4, 6, 8));
+        assert_eq!(r2.dir, vector(0, 1, 0));
+    }
+
+    #[test]
+    fn scaling_a_ray() {
+        let r = ray(point(1, 2, 3), vector(0, 1, 0));
+        let m = scaling(2, 3, 4);
+        let r2 = r.transform(m);
+        assert_eq!(r2.origin, point(2, 6, 12));
+        assert_eq!(r2.dir, vector(0, 3, 0));
     }
 }
